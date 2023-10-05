e837e8381ccb4a22ddea07e8b522ebe0be5c9822
Your Name <you@example.com>
2023-10-05 18:18:00 -0500

    styles restored

--- .history/src/components/Navigation_20231004191327.js
+++ .history/src/components/Navigation_20231004191327.js
@@ -2,14 +2,22 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 const Navigation = () => {
     return (
+<<<<<<< HEAD
         <div className="nav-wrapper">
+=======
+        <nav>
+>>>>>>> fdf389b1638a4083eb289d287e0bcb6fc75ac27f
             <ul>
               <li><Link to="#">UI Design</Link></li>
               <li><Link to="#">Front-end</Link></li>
               <li><Link to="#">Back-end</Link></li>
               <li><Link to="#">Lainnya</Link></li>
             </ul>
+<<<<<<< HEAD
         </div>
+=======
+        </nav>
+>>>>>>> fdf389b1638a4083eb289d287e0bcb6fc75ac27f
     );
 };
 export default Navigation;
